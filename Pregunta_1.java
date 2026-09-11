cde6e97d49c254ab69bf11ba7bc9f1a6ae2b21cd
AndiaCaceresJohn <mvtvtv@gmail.com>
2026-09-10 20:01:37 -0500

    Create Pregunta_1.java
    Implementa CrearMascotas

---
+++ Pregunta_1.java
@@ -0,0 +1,12 @@
+public class Pregunta_1 {
+
+    public static Mascota[] CrearMascotas() {
+        return new Mascota[] {
+            new Mascota("Max", 5, "Carlos"),
+            new Mascota("Bella", 2, "Ana"),
+            new Mascota("Rocky", 7, "Pedro"),
+            new Mascota("Luna", 1, "Maria"),
+            new Mascota("Toby", 4, "Juan")
+        };
+    }
+}
